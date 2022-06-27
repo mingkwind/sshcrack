6f98f90986440bda4d4d96e5a28a21c9b78ff05d
mingkwind <mingkwind@gmail.com>
2022-06-27 17:09:31 +0800

    sshcrack

--- gobuild_darwin.sh
+++ gobuild_darwin.sh
@@ -1,7 +1,8 @@
 go build -o sshcrack
 rm -rf sshcrack_darwin
 mkdir sshcrack_darwin
-cp sshcrack reScanStrs.conf sshcrack_darwin/
+mv sshcrack sshcrack_darwin/
+cp reScanStrs.conf sshcrack_darwin/
 touch sshcrack_darwin/ip_list.txt
 touch sshcrack_darwin/user.txt
 touch sshcrack_darwin/pass.txt

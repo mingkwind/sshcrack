55c05306e172f551eadfe4ce955addaad293b676
mingkwind <mingkwind@gmail.com>
2022-06-27 15:29:44 +0800

    sshcrack

--- .history/gobuild_20220627003306.sh
+++
@@ -1,7 +0,0 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o sshcrack
-rm -rf sshpwd_crack
-mkdir sshpwd_crack
-cp sshcrack sshcrack/
-touch sshcrack/ip_list.txt
-touch sshcrack/user.txt
-touch sshcrack/pass.txt
--- .history/gobuild_20220627003835.sh
+++
@@ -1,7 +0,0 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o sshcrack
-rm -rf sshpwd_crack
-mkdir sshpwd_crack
-cp sshcrack reScanStrs.conf sshpwd_crack/
-touch sshpwd_crack/ip_list.txt
-touch sshpwd_crack/user.txt
-touch sshpwd_crack/pass.txt
